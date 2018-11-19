c1b69cd7f1086bfab56614fa4fb999ab013424ba
Adam <aschuster@gladson.com>
2018-11-19 16:04:14 +0200

    event pushing fixes

--- src/cooladata/cooladata.js
+++ src/cooladata/cooladata.js
@@ -4,17 +4,15 @@ class Cooladata {
   sendEvent(events){
     let jsonRec = JSON.stringify(events);
     const API_KEY = 'syyd0dfyl3ueczcgoosugsex7excgexs';
-    const PROJECT_ID = '121109';
     axios({
       method:'post',
-      url: `https://api.cooladata.com/v3/${PROJECT_ID}/track`,
+      url: `https://api.cooladata.com/v3/${API_KEY}/track`,
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Token ${API_KEY}`//,
         //'crossDomain': true
       },
       data: {
-        "events": [jsonRec]
+        "events": [events]
         //[
         //   {"event_name":"page_load","user_id":"1001","event_timestamp_epoch":1436798955476,"first_name":"john","last_name":"doe"}//,
         //   // {"event_name":"login","user_id":"1005","event_timestamp_epoch":1436798956879,"first_name":"johanna","last_name":"jones"}
